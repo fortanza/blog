7b2f78a905e5cf4fa9930879f6aa0a14a4474f14
fortanza <undertaker040880@gmail.com>
2021-06-16 17:27:06 +0200

    structure du blog

---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+    return <h1>Acceuil</h1>
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+//librairie
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'; //initialisation Routeur
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
